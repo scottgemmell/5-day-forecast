a24441ffbf66a84df1ad098677b1186f3e3aeb53
Scott Gemmell <scott.gemmell@mac.com>
2018-12-31 12:00:07 +0000

    Removed duplicate <Day /> component

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Col } from 'react-bootstrap';
 import cloudyDay from "../assets/svgs/vendor/amcharts/animated/cloudy-day-1.svg";
-import rainyDay from "../assets/svgs/vendor/amcharts/animated/rainy-6.svg";
 
 class Day extends Component {
     render() {
@@ -32,40 +31,6 @@ class Day extends Component {
                                 </dt>
                                 <dd>
                                     Sunny
-                                </dd>
-                                <dt>
-                                    Description:
-                                </dt>
-                                <dd>
-                                    Lorem ipsum dolor set elit
-                                </dd>
-                                <dt>
-                                    Temperature:
-                                </dt>
-                                <dd>
-                                    37
-                                </dd>
-                            </dl>
-                        </Col>
-                    </div>
-
-                    <div className="slab u-bgcolor-b">
-                        <Col xs={12} sm={4} className="slab__head u-bgcolor-a">
-                            <img src={rainyDay} alt="Raining" />
-                        </Col>
-                        <Col xs={12} sm={8} className="slab__body u-bgcolor-a">
-                            <dl>
-                                <dt>
-                                    Day:
-                                </dt>
-                                <dd>
-                                    Tuesday
-                                </dd>
-                                <dt>
-                                    Weather:
-                                </dt>
-                                <dd>
-                                    Raining
                                 </dd>
                                 <dt>
                                     Description:
