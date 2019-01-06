8121e6cba4f0d1c478c543d8e3fe9e1947d858c5
Scott Gemmell <scott.gemmell@mac.com>
2019-01-06 09:41:32 +0000

    api error scenario

--- src/redux/actions/api.actions.js
+++ src/redux/actions/api.actions.js
@@ -16,7 +16,7 @@ export const apiSuccess = (response, feature) => ({
 	meta: { feature }
 });
 
-export const apiError = ({ error, feature }) => ({
+export const apiError = (error, feature) => ({
 	type: `${feature} ${API_ERROR}`,
 	payload: error,
 	meta: { feature }
