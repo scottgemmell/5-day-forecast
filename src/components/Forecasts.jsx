08a494b716e2dd92a9ae0048c6df3028c60d12d8
Scott Gemmell <scott.gemmell@mac.com>
2019-10-31 08:29:48 +0000

    <Forecasts /> class to hook

--- src/components/Forecasts.jsx
+++ src/components/Forecasts.jsx
@@ -1,79 +1,36 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect, useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import spinner from "../assets/svgs/spinner.svg";
 
 import * as R from "ramda";
 import ForecastDaily from "./ForecastDaily";
 
-export class Forecasts extends Component {
+export const Forecasts = () => {
+	
+	const loading = useSelector(state => state.loading);
+	const forecasts = useSelector(state => state.forecasts);
+	const list = useSelector(state => state.forecasts.list);
 
-	render() {
-		const { forecasts, forecasts: { list }, loading } = this.props;
-
-		if (R.isEmpty(forecasts)) {
-			return (<div></div>);
-		}
-
-		if (loading === true) {
-			return (<div className="u-spinner">
-				<img src={spinner} alt="Loading..." />
-			</div>);
-		}
-
-		return (
-			<section>
-				<h2 className="section-title">
-					Forecasts <em>for</em> {this.props.forecasts.city.name}
-				</h2>
-				<div className="forecasts l-panels l-panels@small l-panels@medium l-panels@large">
-					{list.map((item, i) => (<ForecastDaily {...item} key={i} />))}
-				</div>
-			</section>
-		);
+	if (R.isEmpty(forecasts)) {
+		return (<div></div>);
 	}
-}
 
-Forecasts.defaultProps = {
-	forecasts: {
-		list: "",
-		city: {
-			name: "",
-		}
+	if (loading === true) {
+		return (<div className="u-spinner">
+			<img src={spinner} alt="Loading..." />
+		</div>);
 	}
+
+	return (
+		<section>
+			<h2 className="section-title">
+				Forecasts <em>for</em> {forecasts.city.name}
+			</h2>
+			<div className="forecasts l-panels l-panels@small l-panels@medium l-panels@large">
+				{list.map((item, i) => (<ForecastDaily {...item} key={i} />))}
+			</div>
+		</section>
+	);
 };
 
-Forecasts.propTypes = {
-	forecasts: PropTypes.shape({
-		list: PropTypes.arrayOf(
-			PropTypes.shape({
-				dt_txt: PropTypes.string.isRequired,
-				weather: PropTypes.arrayOf(
-					PropTypes.shape({
-						main: PropTypes.string.isRequired,
-						description: PropTypes.string.isRequired,
-					})
-				).isRequired,
-				main: PropTypes.shape({
-					temp: PropTypes.string.number,
-				}),
-			}).isRequired,
-			
-		),
-		city: PropTypes.shape({
-			name: PropTypes.string.isRequired,
-		}),
-	}),
-	loading: PropTypes.bool,
-};
-
-const mapStateToProps = (state) => ({
-	forecasts: state.forecasts,
-	// forecastsFailure: state.forecastsFailure,
-	// forecastsLoading: state.forecastsLoading
-	loading: state.ui.loading,
-});
-
-export default connect(
-	mapStateToProps,
-)(Forecasts);
+export default Forecasts;
