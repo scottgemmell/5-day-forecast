9558502c49c8a100b3715642d173945bc81437a2
Scott Gemmell <scott.gemmell@mac.com>
2019-11-22 10:52:21 +0000

    minor

--- src/containers/AppContainer.test.js
+++ src/containers/AppContainer.test.js
@@ -4,14 +4,14 @@ import { shallow } from "enzyme";
 
 const defaultProps = () => ({});
 const setup = (overrideProps = {}) => {
-    const props = Object.assign({}, defaultProps(), overrideProps);
-    const wrapper = shallow(<AppContainer {...props} />);
-    return { wrapper, props };
+	const props = Object.assign({}, defaultProps(), overrideProps);
+	const wrapper = shallow(<AppContainer {...props} />);
+	return { wrapper, props };
 };
 
 describe("<AppContainer />", () => {
-    it("renders", () => {
-        const { wrapper } = setup();
-        expect(wrapper).toMatchSnapshot();
-    });
+	it("renders", () => {
+		const { wrapper } = setup();
+		expect(wrapper).toMatchSnapshot();
+	});
 });
